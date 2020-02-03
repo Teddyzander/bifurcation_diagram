b33ba67f54259d8ea738c971cd7c8ff136cedef8
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2020-02-03 18:03:05 +0000

    Plot a basic logistic map

--- bifurcation_diagram.py
+++ bifurcation_diagram.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def logistic_map(r, x):
     """
     Logistic map function
@@ -8,7 +9,15 @@ def logistic_map(r, x):
     :param x: Current population
     :return: Next population
     """
-    if x > 1:
+    if x.any() > 1:
         raise Exception("x cannot be larger than 1")
     else:
         return r * x * (1 - x)
+
+
+# Create a graph the represent the logistic map of current population against next population
+x = np.linspace(0, 1)
+fig, ax = plt.subplots(1, 1)
+ax.plot(x, logistic_map(2, x), "k")
+
+plt.show()
